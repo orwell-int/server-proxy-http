c6513049456e24e80de33920d15ef39f3273268b
Massimo Gengarelli <massimo.gengarelli@gmail.com>
2014-11-21 22:26:05 +0100

    Admin interface working properly now.

--- src/server_proxy_web/server.clj
+++ src/server_proxy_web/server.clj
@@ -1,6 +1,7 @@
 (ns server-proxy-web.server
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
+            [compojure.handler :as handler]
             [server-proxy-web.views.orwell :as orwell]
             [server-proxy-web.views.admin :as admin]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
@@ -11,11 +12,11 @@
   (GET  "/orwell/html/router/:message" [message & params] (orwell/router-html message params))
   (GET  "/orwell/router/:message" [message & params] (orwell/router message params))
   (GET  "/orwell/admin/" [] (admin/admin))
-  (POST "/orwell/admin/" [params] (admin/admin params))
+  (POST "/orwell/admin/" [command] (admin/admin command))
 
   ;; Default routes
   (route/resources "/orwell/resources/")
   (route/not-found "Not found"))
 
 (def app
-  (wrap-defaults app-routes site-defaults))
+  (handler/site app-routes))
--- src/server_proxy_web/views/admin.clj
+++ src/server_proxy_web/views/admin.clj
@@ -4,14 +4,14 @@
         [server-proxy-web.models.messages :as messages]
         [flatland.protobuf.core :as protobuf]
         [org.zeromq.clojure :as zmq]
-        [ring.util.anti-forgery :refer [anti-forgery-field]]
+        [ring.middleware.anti-forgery :as af]
+        [ring.util.anti-forgery]
         [clojure.data.json :as json :only [write-str read-str read-json]]
         [clojure.java.io :as io]
         [byte-streams]
         [clojure.string :only [split]]))
 
-
-(def main-page
+(defn build-page [& {:keys [response error]}]
   (page/html5
    [:head [:title "Orwell Router | Admin Interface"]
     (page/include-css "/orwell/resources/css/simple.css")]
@@ -19,25 +19,33 @@
     [:div#container
      [:div#header [:h1 "Orwell Router | Admin Interface"]]
      [:form#main {:action "/orwell/admin/" :method "post"}
-      [:input.command {:type "text" :name "command" :placeholder "Issue a command"}]
-      (anti-forgery-field)
-      [:input {:type "submit" :name "submit" :value "Go!"}]]]]))
+      [:input.command {:type "text"
+                       :name "command"
+                       :size "40"
+                       :placeholder "Issue a command"}] [:br]
+      [:input {:type "submit" :name "submit" :value "Go!"}]]
+     (if (or response error)
+       [:div#response [:h3.response "The server answers"]
+        (if response
+          [:code response])
+        (if error
+          "An error occured " error)])]]))
 
-(defn parse-parameters
-  [params] "list robot")
 
 (defn admin
   "This is the main view for administrating the server game"
-  ([] main-page)
+
+  ;; Get interface is quite simple
+  ([] (build-page))
 
 
-  ([params]
+  ;; This is for when we receive a POST
+  ([command]
   (let [context (zmq/make-context 1)
         socket (zmq/make-socket context zmq/+req+)
-        server-address "tcp://localhost:9003"
-        command (parse-parameters params)]
+        server-address "tcp://localhost:9003"]
     (zmq/connect socket server-address)
     (if (zmq/send- socket (messages/string-to-bytes command))
       (let [response (bytes-to-string (zmq/recv socket))]
-        (println "Received \n" response))
-      (println "Message NOT sent")))))
+        (build-page :response response))
+      (build-page :error "Communication error ?")))))
